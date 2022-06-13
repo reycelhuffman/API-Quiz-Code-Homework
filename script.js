f07e7757227cf2cc73fdeb7ed351ef9382a4e3e1
Reycel Huffman <reycel.huffman@outlook.com>
2022-06-12 21:49:57 -0500

    trying to get my quiz to start

--- script.js
+++ script.js
@@ -1,91 +1,114 @@
 let quizTimer = document.querySelector("#quizTimer");
 let startQuiz = document.querySelector("#startQuiz");
+let quizContainer = document.querySelector("#main");
+let question = document.querySelector("#question");
 let A = document.querySelector("#A");
 let B = document.querySelector("#B");
 let C = document.querySelector("#C");
 let D = document.querySelector("#D");
+let scoreContainer = document.querySelector("#score");
+let score = 0;
 let timer = 80;
 
 
 
-let question = document.querySelector("#question");
-let choiceA = document.querySelector(".A"); 
-let choiceB = document.querySelector(".B"); 
-let choiceC = document.querySelector(".C"); 
-let choiceD = document.querySelector(".D"); 
 let currentQuestion = 0 
 let questionsCorrect = 0  
 
 let questions = [
     {
         question: "What is Katniss sister's full name?",
-        choices: ["Pamela", "Primly", "Prim","Primrose"],
+        A: "Pamela", 
+        B: "Primly", 
+        C: "Prim",
+        D: "Primrose",
         answer: "Primrose"
     },
     {
         question: "Why does the Capital hold the Hunger Games?",
-        choices: ["As a way to control the size of the population", "To keep the districts happy", "Because they are part of a religious festival",
-        "To remind the districts that they are powerless against it"],
+        A: "As a way to control the size of the population", 
+        B: "To keep the districts happy", 
+        C: "Because they are part of a religious festival",
+        D: "To remind the districts that they are powerless against it",
         answer: "To remind the districts that they are powerless against it"
     },
     {
         question: "Under what circumstances did Katniss first meet Peeta?",
-        choices: [ "Katniss was looking for food and Peeta gave her bread", "They were both in the woods hunting", "They did a project at school together", "Peeta was injured and Katniss mother helped him"], 
+        A:  "Katniss was looking for food and Peeta gave her bread", 
+        B: "They were both in the woods hunting", 
+        C: "They did a project at school together", 
+        D: "Peeta was injured and Katniss mother helped him", 
         answer: "Katniss was looking for food and Peeta gave her bread"
     },
     {
         question: "What does Rue point out to Katniss that let her get away form the group of Career Tributes?",
-        choices: ["A sheaf of arrows", "A tracker jacker nest", "A boulder", "A mockinjay nest"],
+        A: "A sheaf of arrows", 
+        B: "A tracker jacker nest", 
+        C: "A boulder", 
+        D: "A mockingjay nest",
         answer: "A tracker jacker nest"
     },
     {
-        question: "How do Katniss and Peeta force the Capitol to declare them both winners?",
-        answer: ["They threaten to run away",
-        "They threaten that the winner will tell about everything",
-        "They threaten to commit suicide",
-        "They threaten to cause a rebellion against the Capitol"],
+        question: "How do Katniss and Peeta force the Capitol to declare them both winners?", 
+        A: "They threaten to run away",
+        B: "They threaten that the winner will tell about everything",
+        C: "They threaten to commit suicide",
+        D: "They threaten to cause a rebellion against the Capitol",
         answer: "They threaten to commit suicide"
     },
     
 ]
 
 startButton.addEventListener('click', startQuiz);
-    function startQuiz(){
-        quizTimer();
+    function startQuiz() {
+        timerStart();
         showQuestion();
 
-        document.querySelectorAll(".answerButton").forEach(item) => {
+        document.querySelectorAll(".answerButton").forEach((item) => {
             item.addEventListener("click", (event) => {
                 if (
-                    event.currentTarget.innerText === myQuestion[currentQuestion].answer
+                    event.currentTarget.innerText === myQuestions[currentQuestion].answer
                 ) {
-                    score++;
+                    score ++;
                 } else {
                     startTime -= 10;
                 }
-                currentQuestion++;
+                currentQuestion ++;
                 if (currentQuestion > 3) {
                     endQuiz();
                 } else {
                     showQuestion();
                 }
             });
-        }
+        });
     }
 function endQuiz() {
-    let nameInput = document.createElement("input");
+    let typeName = document.createElement("name");
     let scoreSubmit = document.createElement("button");
     quizContainer.innerHTML = "";
-    nameInput.setAttribute("id", "nameInput");
+    typeNAme.setAttribute("id", "typeNAme");
     scoreSubmit.setAttribute("id", "scoreSubmit");
     scoreSubmit.innerText = "Submit";
-    quizContainer.append(nameInput);
+    quizContainer.append(typeNAme);
     quizContainer.append(scoreSubmit);
     scoreSubmit.addEventListener("Click", submitScore);
     document.getElementsById("startButton").remove();
     startTime = 0;
 }
 
+function submitScore() {
+    let scores = []
+    let name = document.querySelector("typeName").value;
+    if (localStorage.getItem ("scores")) {
+        scores = JSON.parse(localStorage.getItem("scores"));
+    }
+scores.push({
+    name: name,
+    score: score,
+});
+localStorage.setItem("scores", JSON.stringify(scores));
+displayScores(scores);
+}
 function displayScores(scores) {
     quizContainer.innerHTML = "";
     for (entry of scores) {
@@ -97,7 +120,7 @@ function displayScores(scores) {
 function quizTimer() {
     let timeInterval;
     timeInterval = setInterval(function () {
-        if (startTime > = 1) {
+        if (startTime >= 1) {
             startQuiz --;
             timer.textContent = startTime + "Second left";
         }
